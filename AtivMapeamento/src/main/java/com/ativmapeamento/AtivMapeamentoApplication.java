43f8f4d2af39ce89b4da818466f58fdae4b3f4d9
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-03-14 09:04:58 -0300

    Add files via upload

---
+++ AtivMapeamento/src/main/java/com/ativmapeamento/AtivMapeamentoApplication.java
@@ -0,0 +1,13 @@
+package com.ativmapeamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivMapeamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivMapeamentoApplication.class, args);
+	}
+
+}
---
+++ AtivMapeamento/src/test/java/com/ativmapeamento/AtivMapeamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativmapeamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivMapeamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
